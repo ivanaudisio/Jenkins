a16532bec9dca4a9f46e580a111afccb7b94540c
ivanaudisio <ivan@forest-technologies.co.uk>
2016-11-29 14:18:20 +0000

    Works on an item if it has the specified method instead of doing it based on the class that was instantiated.

--- getTimerTriggerJobs.groovy
+++ getTimerTriggerJobs.groovy
@@ -4,44 +4,42 @@
 import hudson.model.*
 import hudson.triggers.*
 
-jobs = Jenkins.instance.getAllItems()
-
+jobs = Jenkins.instance.getAllItems()	// Get all jobs
+def count = 0				// Counts number of jobs found using CRON
+  
 println("------------------------------------------------------------------------")
 println("The following jobs are being executed with a CRON job")
 println("------------------------------------------------------------------------")
 println("")
 
 jobs.each {job ->
-  
-	if (job instanceof com.cloudbees.hudson.plugins.folder.Folder) {
-    	inspect = false
-	} else if (job instanceof com.cloudbees.hudson.plugins.modeling.impl.jobTemplate.JobTemplate) {
-        inspect = false
-    } else {
-        inspect = true
-    }
 
-    if (inspect) {
-      
-        job.triggers.each{ descriptor, trigger ->
-            if(trigger instanceof TimerTrigger) {
-            println("Name        : ${job.name}")
-            println("Class       : ${job.class}")
-            println("Root Dir    : ${job.rootDir}")
-            println("URL         : ${job.url}")
-            println("Absolute URL: ${job.absoluteUrl}")
-            println("Trigger     : "+trigger.spec)
-            println("")
-            println("")
-              
-              //job.removeTrigger descriptor
-              //job.save()
-              //job.addTrigger(new TimerTrigger("$m $hr * * *"))
-              //job.save()
-                
-                           
-            }
-        }
-    }
-
+	// Check if Job contains the method getTriggers (skips templates and folders)
+	if (job.metaClass.getMetaMethod("getTriggers")) {
+		// Gets CRON triggers from Job
+		job.triggers.each{ descriptor, trigger ->
+			// Verifies that the specs have been entered and prints the job's information
+			if(trigger instanceof TimerTrigger) {
+				if (trigger.spec) {
+					count ++
+					println("Name        : ${job.name}")
+					println("Class       : ${job.class}")
+					println("Root Dir    : ${job.rootDir}")
+					println("URL         : ${job.url}")
+					println("Absolute URL: ${job.absoluteUrl}")
+					println("Trigger     : "+trigger.spec)
+					println("")
+					println("")
+				}
+				//The following code can be used to modify the triggers if needed.
+				//job.removeTrigger descriptor
+				//job.save()
+				//job.addTrigger(new TimerTrigger("$m $hr * * *"))
+				//job.save()
+			}
+		}
+	}
 }
+println("------------------------------------------------------------------------")
+println("${count} job(s) are being executed with CRON")
+println("------------------------------------------------------------------------")
