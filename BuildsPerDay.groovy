4554a5c2f41c9c06e43e03769c901ec8d021868d
ivanaudisio <ivan@forest-technologies.co.uk>
2016-12-02 15:09:49 +0000

    Corrected time frame starting from 00:00:00 to 23:59:59
    Changed code to be cofigurable
    Added job URL
    Added job Absolute URL
    Added job Root Dir

--- BuildsPerDay.groovy
+++ BuildsPerDay.groovy
@@ -4,50 +4,98 @@
 import groovy.time.TimeCategory
 import java.text.SimpleDateFormat;
 
-jobs = Jenkins.instance.getAllItems()	// Get all jenkins items
+// TO DO
+// Add validation when entering stratTime and endTime (end needs to be higher)
+// Add specific date (remember to update how header is shown)
+// Able to set parameters to show in the CSV report
+// validate startTime and endTime format
 
-def days = 3
-def headerString = "Job;Template"
-def totalBuilds = 0
+// ***************************************************
+// CONFIGURATION
+// ***************************************************
+def days = 3				// Ammount of days to report (Including today)
+def startTime = ""			// If left blank by default it starts at the beggining of the day 00:00:00 [24 hour format e.g. 06:20:00]
+def endTime = ""			// If left blank by default it ends at the end of the day 23:59:00 [24 hour format e.g. 14:30:00]
+//def specificDate = ""			// If a value is entered it overrides the ammount of days to search for (e.g. 28-11-2016)
+def showTemplate = false
+def showJobRootDir = false
+def showJobURL = false
+def showJobAbsoluteURL = false
 
-for (i = days; i > 0; i--) {
+// ***************************************************
+// INITIALIZE DEFAULT PARAMETERS
+// ***************************************************
+if (!startTime) {startTime = "00:00:00"}
+if (!endTime) {endTime = "23:59:50"}
+
+// ***************************************************
+// CREATE CSV HEADER
+// ***************************************************
+def headerString = "Job"					// Initialize header for CSV report
+showTemplate ? headerString += ";Template":""			// Add Tempalte to header
+showJobRootDir ? headerString += ";Job Root Dir":""		// Add Job URL to header
+showJobURL ? headerString += ";Job URL":""			// Add Job Path to header
+showJobAbsoluteURL ? headerString += ";Job Absolute URL":""	// Add Job Path to header
+for (i = days; i > 0; i--) {					// Add days to header	
   header = new Date() - (i - 1)
   header = header.format( 'd-M-yyyy' )
   headerString += ";" + header
 }
+(days > 1) ? headerString += ";Total Builds":""			// Add Total Number of builds to header
+println("${headerString}")					// Print header
 
-println("${headerString};Total Builds")
+// ***************************************************
+// BEGING GATHERING JOBS INFORMATION FOR GIVEN DATES
+// ***************************************************
 
+def jobs = Jenkins.instance.getAllItems()			// Get all jenkins items
 
 jobs.each {job ->
 
 	// Verify that the item contains the method getLastBuild (avoids templates and folders) 
 	if (job.metaClass.getMetaMethod("getLastBuild")) {
 
-		templateName = ""
+		template = ""
 		buildString = ""
 		totalBuilds = 0
 		
+		// Get Builds per day
 		for (i = days; i > 0; i--) {
-			count = 0
-			start = new Date() - (i - 1)
-			start = start.getTime()
-			end = new Date() - i
-			end =  end.getTime();      
-			builds = job.getBuilds().byTimestamp(end, start)
-			builds.each{build -> count ++}
-			buildString += ";${count}"  
+			count = 0									// Restart build count per Job
+          
+			startDate = new Date() - (i - 1)						// Set starting day
+			startDate = startDate.format( 'd-M-yyyy' )					// Set date format
+			startDate = Date.parse("d-M-yyyy HH:mm:ss", "${startDate} ${startTime}")	// Set starting time
+			startDate = startDate.getTime()							// Get Timestamp for the formed date
+
+			endDate = new Date() - (i - 1)							// Set ending day
+			endDate = endDate.format( 'd-M-yyyy' )						// Set date format
+			endDate = Date.parse("d-M-yyyy HH:mm:ss", "${endDate} ${endTime}")       	// Set ending time   
+			endDate =  endDate.getTime();  							// Get Timestamp for the formed date
+          
+			builds = job.getBuilds().byTimestamp(startDate,endDate)				// Get all builds with both timestamps
+			builds.each{build -> count ++}							// Count number of builds found
+			buildString += ";${count}"
 			totalBuilds += count			
 		}
 		
-		// Determines if the Job is build from a Template
-		def model = com.cloudbees.hudson.plugins.modeling.impl.entity.EntityInstance.from(job)
-		if (model) {
-			templateName = "${model.modelId}"
+		// Valite if job is an instance of a template
+		if (showTemplate) {
+			def model = com.cloudbees.hudson.plugins.modeling.impl.entity.EntityInstance.from(job)
+			if (model) {
+				template = "${model.modelId}" // Get template path
+			}
 		}
 
-		// Print Row in CSV format "Job,Tempalte,Day1,Day2,...,Total Builds"
-		println("${job.name};${templateName}${buildString};${totalBuilds}")
+		// Print requested Job information in one row (CSV format)
+		newRow = "${job.name}"						// Add Job name
+		showTemplate ? newRow += ";${template}":""			// Add Job template
+		showJobRootDir ? newRow += ";${job.rootDir}":""			// Add Job root directory
+		showJobURL ? newRow += ";${job.url}":""				// Add Job url
+		showJobAbsoluteURL ? newRow += ";${job.absoluteUrl}":""		// Add Job absolute url
+		newRow += "${buildString}"					// Add days
+		(days > 1) ? newRow += ";${totalBuilds}":""			// Add total builds in given days
+		println("${newRow}")						// Print all parameters
 	}
 }
 println("")
