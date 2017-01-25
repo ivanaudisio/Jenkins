edc6c0d940be2c5769c4404698b4a331a0ce8f39
ivanaudisio <ivan.audisio@gmail.com>
2017-01-25 08:46:07 +0000

    added template

--- getRetentionPolicy.groovy
+++ getRetentionPolicy.groovy
@@ -1,1 +1,28 @@
-def jobs = Jenkins.instance.getAllItems() // Get all jenkins items  //Headerprintln ("Job Name,URL,Days to Keep,Number to keep,Artifact Days to keep,Artifact Num. to keep")jobs.each { job ->    template = "" // Verify that the item contains the method getLastBuild (avoids templates and folders)   if (job.metaClass.getMetaMethod("getLastBuild")) {        // Get Retention policy  def d = job.buildDiscarder       // Print Line with information found       if (d){          println("${job.name},${job.absoluteUrl},${d.daysToKeep},${d.numToKeep},${d.artifactDaysToKeep},${d.artifactNumToKeep}")        }else{          println("${job.name},${job.absoluteUrl},No Retention Policy")        }    }}println ""
+def jobs = Jenkins.instance.getAllItems() // Get all jenkins items 
+ 
+//Header
+println ("Job Name,URL,Days to Keep,Number to keep,Artifact Days to keep,Artifact Num. to keep")
+jobs.each { job -> 
+
+  	template = ""
+	// Verify that the item contains the method getLastBuild (avoids templates and folders)  
+	if (job.metaClass.getMetaMethod("getLastBuild")) { 
+      	
+        // Get template if the job is an instance
+        def model = com.cloudbees.hudson.plugins.modeling.impl.entity.EntityInstance.from(job)
+        if (model) {
+            template = "${model.modelId}" // Get template path
+        }
+
+        // Get Retention policy
+        def d = job.buildDiscarder
+        
+      	// Print Line with information found
+      	if (d){
+          println("${job.name},${template},${job.absoluteUrl},${d.daysToKeep},${d.numToKeep},${d.artifactDaysToKeep},${d.artifactNumToKeep}")
+        }else{
+          println("${job.name},${template},${job.absoluteUrl},No Retention Policy")
+        }
+    }
+}
+println ""
