19d95ee9a18c781ab8408983a5f2083dee1458f0
ivanaudisio <ivan.audisio@gmail.com>
2017-01-27 13:42:50 +0000

    added folder size for job

--- getRetentionPolicy.groovy
+++ getRetentionPolicy.groovy
@@ -1,27 +1,36 @@
 def jobs = Jenkins.instance.getAllItems() // Get all jenkins items 
  
 //Header
-println ("Job Name,URL,Builds: Days to Keep,Builds: Num. to keep,Artifacts: Days to keep,Artifacts: Num. to keep,Template")
+println ("Job Name,URL,Builds: Days to Keep,Builds: Num. to keep,Artifacts: Days to keep,Artifacts: Num. to keep,Template,Size Kilobytes")
 jobs.each { job -> 
 
   	template = ""
+  	model = ""
 	// Verify that the item contains the method getLastBuild (avoids templates and folders)  
 	if (job.metaClass.getMetaMethod("getLastBuild")) { 
       	
         // Get template if the job is an instance
-        def model = com.cloudbees.hudson.plugins.modeling.impl.entity.EntityInstance.from(job)
+      	try {
+        	def model = com.cloudbees.hudson.plugins.modeling.impl.entity.EntityInstance.from(job)
+        } catch (MissingPropertyException e){
+        	model = ""
+        }
         if (model) {
             template = "${model.modelId}" // Get template path
         }
+      	
+      	// Get Build folder size for job
+       	buildFolder = new File("${job.rootDir}")
+		buildFolderSize = buildFolder.directorySize() * 0.001
 
         // Get Retention policy
         def d = job.buildDiscarder
         
       	// Print Line with information found
       	if (d){
-          println("${job.name},${job.absoluteUrl},${d.daysToKeep},${d.numToKeep},${d.artifactDaysToKeep},${d.artifactNumToKeep},${template}")
+          println("${job.name},${job.absoluteUrl},${d.daysToKeep},${d.numToKeep},${d.artifactDaysToKeep},${d.artifactNumToKeep},${template},${buildFolderSize}")
         }else{
-          println("${job.name},${job.absoluteUrl},No Retention Policy,,,,${template}")
+          println("${job.name},${job.absoluteUrl},No Retention Policy,,,,${template},${buildFolderSize}")
         }
     }
 }
