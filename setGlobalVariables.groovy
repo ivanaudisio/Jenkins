63a8c14207c43ba17e2abe16ddb4d65e84e2dcf7
ivanaudisio <ivan.audisio@gmail.com>
2017-03-30 00:36:31 +0100

    Create setGlobalVariables.groovy

---
+++ setGlobalVariables.groovy
@@ -0,0 +1,21 @@
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+  newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+  globalNodeProperties.add(newEnvVarsNodeProperty)
+  envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+  envVars = envVarsNodePropertyList.get(0).getEnvVars()
+
+}
+
+envVars.put("FOO1", "foo")
+envVars.put("FOO2", "foo")
+envVars.put("FOO3", "foo")
+
+instance.save()
