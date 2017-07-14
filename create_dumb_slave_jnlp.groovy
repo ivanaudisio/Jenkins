3ee20dc263ce73d90162aa34db7401151974c87b
ivanaudisio <ivan.audisio@gmail.com>
2017-07-14 20:20:02 +0100

    Update create_dumb_slave_jnlp.groovy

--- create_dumb_slave_jnlp.groovy
+++ create_dumb_slave_jnlp.groovy
@@ -1,4 +1,15 @@
 import jenkins.model.*
 import hudson.model.*
 import hudson.slaves.*
-Jenkins.instance.addNode(new DumbSlave("{{ ansible_hostname }}-shared","","{{ remote_root_dir }}","{{ nb_slave_executors }}",Node.Mode.NORMAL,"{{ label_string }}",new JNLPLauncher(),new RetentionStrategy.Always(),new LinkedList()))
+
+Jenkins.instance.addNode(
+  new DumbSlave(
+    "test-script",
+    "test slave description",
+    "/export/home/pe-deploy/",
+    "1",
+    Node.Mode.NORMAL,
+    "test-slave-label",
+    new JNLPLauncher(),
+    new RetentionStrategy.Always(),
+    new LinkedList()))
