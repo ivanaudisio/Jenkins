87a97311a2aa124867ac415c2c5525b7c1d22f54
ivanaudisio <ivan.audisio@gmail.com>
2017-10-31 00:09:09 +0000

    Update print-rbac-roles.groovy

--- print-rbac-roles.groovy
+++ print-rbac-roles.groovy
@@ -1,18 +1,22 @@
 import jenkins.model.Jenkins
 import hudson.model.User
 
-allUsers = User.getAll()
-adminList = []
-test = Jenkins.instance.getAuthorizationStrategy()
+authorization = Jenkins.instance.getAuthorizationStrategy()
 
-test.metaClass.methods*.name.sort().unique()
+roles = authorization.getGrantedRoles("globalRoles")
 
-roles = test.getGrantedRoles("globalRoles")
+for (entry in roles) {
+	role = entry.key
+	users = entry.value
+    println role.getName() + " = " + users  
+}
+println '----------------------------'
 
-    for (entry in roles) {
-        role = entry.key
-        users = entry.value
-      
-      println role.getName() + " = " + users
-      
+for (entry in roles) {
+	role = entry.key
+  	println role.getName()
+    role.permissions.each {
+    	println it.id
     }
+  	println '----------------------------'
+}
