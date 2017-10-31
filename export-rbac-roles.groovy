00527094fcd19c21b7361f2a0b419e83c25bc57e
ivanaudisio <ivan.audisio@gmail.com>
2017-10-31 01:14:44 +0000

    Create export-rbac-roles.groovy

---
+++ export-rbac-roles.groovy
@@ -0,0 +1,82 @@
+authorization = Jenkins.instance.getAuthorizationStrategy()
+
+roles = authorization.getGrantedRoles("globalRoles")
+
+println '''
+import hudson.*
+import hudson.security.*
+import jenkins.model.*
+import java.util.*
+import com.michelin.cio.hudson.plugins.rolestrategy.*
+import java.lang.reflect.*
+import java.util.logging.*
+import groovy.json.*
+'''
+for (entry in roles) {
+	role = entry.key
+  	println 'globalRole_' + role.getName() + " = '" + role.getName() + "'"
+}
+
+println '\ndef access = ['
+for (entry in roles) {
+	role = entry.key
+	users = entry.value
+    println role.getName() + " : " + users  
+}
+println ']\n'
+
+
+for (entry in roles) {
+	role = entry.key
+    println "def permissions_" + role.getName() + " = [" 
+    role.permissions.each {
+    	println '"' + it.id + '",'
+    }
+    println ']'
+}
+
+println '''
+// Set Role Based Access Control Authorization strategy
+def roleBasedAuthenticationStrategy = new RoleBasedAuthorizationStrategy()
+Jenkins.instance.setAuthorizationStrategy(roleBasedAuthenticationStrategy)
+
+Constructor[] constrs = Role.class.getConstructors();
+for (Constructor<?> c : constrs) {
+  c.setAccessible(true);
+}
+
+// Make the method assignRole accessible
+Method assignRoleMethod = RoleBasedAuthorizationStrategy.class.getDeclaredMethod("assignRole", String.class, Role.class, String.class);
+assignRoleMethod.setAccessible(true);
+'''
+
+for (entry in roles) {
+	role = entry.key
+    roleName = role.getName()
+
+    println '''Set<Permission> ''' + roleName + '''PermissionSet = new HashSet<Permission>();
+adminPermissions.each { p ->
+	def permission = Permission.fromId(p);
+	if (permission != null) {
+   		''' + roleName + '''PermissionSet.add(permission);
+ 	} else {
+   		println("${p} is not a valid permission ID (ignoring)")
+ 	}
+}
+'''
+}
+
+for (entry in roles) {
+	role = entry.key
+println 'Role ' + role.getName() + 'Role = new Role(globalRole_' + role.getName() + ', ' + role.getName() + 'PermissionSet);'
+println 'roleBasedAuthenticationStrategy.addRole(RoleBasedAuthorizationStrategy.GLOBAL, ' + role.getName() + 'Role);\n'
+}
+
+for (entry in roles) {
+	role = entry.key
+println '''access.''' + role.getName() + '''.each { l ->
+  println("Granting ''' + role.getName() + ''' to ${l}")
+  roleBasedAuthenticationStrategy.assignRole(RoleBasedAuthorizationStrategy.GLOBAL, ''' + role.getName() + '''Role, l);  
+}'''
+
+}
