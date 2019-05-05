76ae43722ff3a012d0a24b3f5e3de310f9d74d6e
ivanaudisio <ivan.audisio@gmail.com>
2019-05-05 23:16:00 +0100

    adding json configuration

--- createPipelineJob.groovy
+++ createPipelineJob.groovy
@@ -1,16 +1,30 @@
 import hudson.plugins.git.*;
+import groovy.json.JsonSlurper
 
-scmURL = 'git@github.com:dermeister0/Tests.git'
-scmName = null
-scmRefspec = null
-scmCredential = 'jenkins-credential'
-scmBranch = "*/master"
-jenkinsfileName = "Jenkinsfile"
-pipelineName = "Example pipeline"
+// Example json configuration for jobs (this should be in a repo)
+def restResponse = '''[
+	{"url":"git@github.com:dermeister0/Tests.git", "branch":"*/master", "pipelineName":"tes1"},
+	{"url":"git@github.com:dermeister0/Tests.git", "branch":"*/master", "pipelineName":"test2"},
+	{"url":"git@github.com:dermeister0/Tests.git", "branch":"*/master", "pipelineName":"test3"},
+	{"url":"git@github.com:dermeister0/Tests.git", "branch":"*/master", "pipelineName":"test4"}]'''
 
-def scm = new GitSCM([new UserRemoteConfig(scmURL, scmName, scmRefspec, scmCredential)], [new BranchSpec(scmBranch)], false, null, null, null, null)
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, jenkinsfileName)
-def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, pipelineName)
-job.definition = flowDefinition
-parent.reload()
+// Parse the response
+def list = new JsonSlurper().parseText( restResponse )
+
+// Creating each pipeline
+list.each { 
+  scmURL = it['url']
+  scmName = null
+  scmRefspec = null
+  scmCredential = 'jenkins-credential'
+  scmBranch = it['branch']
+  jenkinsfileName = "Jenkinsfile"
+  pipelineName = it['pipelineName']
+  
+  def scm = new GitSCM([new UserRemoteConfig(scmURL, scmName, scmRefspec, scmCredential)], [new BranchSpec(scmBranch)], false, null, null, null, null)
+  def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, jenkinsfileName)
+  def parent = Jenkins.instance
+  def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, pipelineName)
+  job.definition = flowDefinition
+  parent.reload()
+}
